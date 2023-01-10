6c5903e76b8b1f59d5642ba920fb6f73ba01bbf6
Suryakanta Dash <sdash8273@gmail.com>
2023-01-10 10:49:34 +0530

    Shortest Element in Array.js

---
+++ ShortestElementinArray.js
@@ -0,0 +1,9 @@
+let arr = [11, 2, 5, 7, 8, 1, 10, 4];
+let shortest = Number.MAX_VALUE;
+
+for (let i = 0; i < arr.length; i++) {
+    if (shortest > arr[i]) {
+        shortest = arr[i];
+    }
+}
+console.log(shortest);
