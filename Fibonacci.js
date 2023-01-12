d1c962ed359094915ac8499d8293a2321a61f391
Suryakanta Dash <sdash8273@gmail.com>
2023-01-12 21:21:10 +0530

    updated

---
+++ Fibonacci.js
@@ -0,0 +1,12 @@
+let number = parseInt(prompt("enter a number"));
+let n1=0, n2=1, next;
+
+let fibo = () => {
+    for(let i = 0; i <= number; i++) {
+        console.log(n1);
+        next = n1 + n2;
+        n1 = n2;
+        n2 = next;
+    }
+}
+fibo();
