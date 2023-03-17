64ecf419748578c6de90210b7a6c90a8e8f7e771
Suryakanta Dash <sdash8273@gmail.com>
2023-03-17 09:36:11 +0530

    updated

--- SecondLargestNoInArray.js
+++ SecondLargestNoInArray.js
@@ -1,13 +1,13 @@
 let arr = [5, 9, 2, 4, 1, 7, 6];
-let largest = Number.MIN_VALUE;
+let Largest = Number.MIN_VALUE;
 let sLargest = Number.MIN_VALUE;
 
 for (let i = 0; i < arr.length; i++){
-    if(largest < arr[i]) {
-        sLargest = largest;
-        largest = arr[i];
+    if(Largest < arr[i]) {
+        sLargest = Largest;
+        Largest = arr[i];
     }
-    if(sLargest < arr[i] && largest > arr[i]) {
+    if(sLargest < arr[i] && Largest > arr[i]) {
         sLargest = arr[i];
     }
 }
