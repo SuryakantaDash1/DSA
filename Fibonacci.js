4b5f8bc0b473b08d15358de44d43ccdc6cb67833
Suryakanta Dash <sdash8273@gmail.com>
2023-03-26 23:54:42 +0530

    update

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,8 +1,8 @@
-let number = parseInt(prompt("enter a number"));
+let Number = parseInt(prompt("enter a number"));
 let n1 = 0, n2 = 1, n3;
 
 let Fibonacci = () => {
-    for(let i =0; i <= number; i++) {
+    for(let i =0; i <= Number; i++) {
         console.log(n1);
         n3 = n1 + n2;
         n1 = n2;
