f551b9dc58f0dfc66b4ea8547b78543b54a15061
Suryakanta Dash <sdash8273@gmail.com>
2023-04-01 22:24:52 +0530

    updated

--- SumOfArray.js
+++ SumOfArray.js
@@ -5,9 +5,15 @@
 // let ans = arr.reduce(sum);
 // console.log(ans);
 
-let arr = [2, 4, 6, 8, 10];
-function double(num){
-    return num*2;
+// let arr = [2, 4, 6, 8, 10];
+// function double(num){
+//     return num*2;
+// }
+// let ans = arr.map(double);
+// console.log(ans);
+let arr = [2, 4, 6, 7, 8];
+function fun(number, index){
+    console.log(`index is ${index} number is ${number}`);
 }
-let ans = arr.map(double);
+let ans = arr.forEach(fun);
 console.log(ans);
