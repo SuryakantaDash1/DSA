547bb19ee71d53714b5e02b13ff807c24d91e178
Suryakanta Dash <sdash8273@gmail.com>
2023-04-02 23:25:44 +0530

    updated

--- SumOfArray.js
+++ SumOfArray.js
@@ -24,3 +24,10 @@
 // let ans = arr.forEach(fun);
 // console.log(ans);
 
+// Sum -
+let arr = [2, 4, 5, 6, 9];
+let sum = 0;
+for(let i = 0; i < arr.length; i++){
+    sum = sum + arr[i];
+}
+console.log(sum);
