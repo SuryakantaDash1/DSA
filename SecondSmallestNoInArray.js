030c343a55c64c271436b937f4bf9047e28a42d9
Suryakanta Dash <sdash8273@gmail.com>
2023-04-06 22:25:03 +0530

    updated

--- SecondSmallestNoInArray.js
+++ SecondSmallestNoInArray.js
@@ -1,6 +1,6 @@
 let arr = [21, 5, 6, 9, 16, 7];
 let Small = Number.MAX_VALUE;
-let secSmall 
+let secSmall = Number.MAX_VALUE;
 for(let i = 0; i < arr.length; i++){
     if(Small > arr[i]){
         secSmall = Small;
