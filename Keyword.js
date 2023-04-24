73bf66a349e53109c34cb056780a57e22bf82168
Suryakanta Dash <sdash8273@gmail.com>
2023-04-24 23:04:45 +0530

    updated

--- Keyword.js
+++ Keyword.js
@@ -1,15 +1,15 @@
 let arr = [2, 4, 5, 6, 8, 1,  9, 10];
 
 let max = Number.MIN_VALUE;
-let Secondmax = Number.MIN_VALUE;
+let SecondMax = Number.MIN_VALUE;
 
 for(let i = 0; i < arr.length; i++){
     if(max < arr[i]){
-        Smax = max;
+        SecondMax = max;
         max = arr[i];
        
     }
-    if(Smax < arr[i] && max > arr[i]){
+    if(SecondMax < arr[i] && max > arr[i]){
         Smax = arr[i];
     }
 }
