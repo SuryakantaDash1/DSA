172d335d32fe0d6da25fe58cffb66c9087a980c4
Suryakanta Dash <sdash8273@gmail.com>
2023-05-01 10:33:32 +0530

    updated

--- DuplicateArray.js
+++ DuplicateArray.js
@@ -4,6 +4,12 @@
 
 
 const arr = [1, 2, 3, 4, 5, 6, 7, 9, 10];
-let Misnum =  (arr.length + 1) * (arr.length + 2)
+let Misnum =  ((arr.length + 1) * (arr.length + 2)) / 2;
+
+for(let i = 0; i < arr.length; i++){
+    Misnum = Misnum - arr[i];
+}
+
+console.log(Misnum);
 
 
