d3d4b77ac96ee45a9819b327c09407410007ec6a
Suryakanta Dash <sdash8273@gmail.com>
2023-05-01 10:31:17 +0530

    updated

--- DuplicateArray.js
+++ DuplicateArray.js
@@ -1,6 +1,9 @@
-const arr = [1, 1, 2, 2, 2, 2, 3, 4, 4, 5, 6, 6, 6, 6, ];
-const DupArr = [...new Set(arr)];
-console.log(DupArr);
+// const arr = [1, 1, 2, 2, 2, 2, 3, 4, 4, 5, 6, 6, 6, 6, ];
+// const DupArr = [...new Set(arr)];
+// console.log(DupArr);
 
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 9, 10];
+let Misnum = 
 
 
