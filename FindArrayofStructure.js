9f26631b0be3e3eceb0fbffb558f09c60887db71
Suryakanta Dash <sdash8273@gmail.com>
2023-05-03 10:44:36 +0530

    updated

---
+++ FindArrayofStructure.js
@@ -0,0 +1,17 @@
+function Alert() {
+          
+    // Array structure
+    var guardians = ["Quill", "Gamora",
+            "Groot", "Rocket", "Drax", 21];
+      
+    // Prints the array elements with a comma
+    // separated but not the structure
+    document.getElementById("d1").innerHTML
+            = "Guardians = " + guardians;
+      
+    // Alerts the window with the array
+    console.log(JSON.stringify(guardians))
+      
+    // Alerts the window with a customized array
+    console.log(JSON.stringify(guardians, 9, 1))
+}
