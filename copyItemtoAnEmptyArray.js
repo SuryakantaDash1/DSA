97c7b84d51bc2f3fa087533c2ad9596500dc451d
Suryakanta Dash <sdash8273@gmail.com>
2023-05-04 10:38:43 +0530

    updated

--- copyItemtoAnEmptyArray.js
+++ copyItemtoAnEmptyArray.js
@@ -1,14 +1,10 @@
 
 	let items = ['abc', 'def', 'xyz'];
 
-	// Display the array items
 	console.log(items);
 
-	// Create a new empty array
 	let new_Array = [];
 
-	// forEach loop to push elements
-	// into new array
 	items.forEach(function (item) {
 		new_Array.push(item);
 	});
