109927317cba74d5f2c17b30cdbc2efde27676b1
Suryakanta Dash <sdash8273@gmail.com>
2023-05-07 22:57:07 +0530

    update

--- ThirdSmallestNoInArray.js
+++ ThirdSmallestNoInArray.js
@@ -7,5 +7,5 @@ nums = [2, 3, 5, 7, 1]
 console.log("Original array : " + nums)
 console.log("Third smallest number: "+test(nums));
 nums = [2, 3, 0, 5, 7, 8, -2, -4]
-console.log("Original array of numbers: "+ nums)
+console.log("Original arrays: "+ nums)
 console.log("Third smallest number of the said array of numbers: "+test(nums));
