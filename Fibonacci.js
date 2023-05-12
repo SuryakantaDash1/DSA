a2fc01bf923c06ed89ad0cb1f78ee947bd54431e
Suryakanta Dash <sdash8273@gmail.com>
2023-05-12 13:03:19 +0530

    update

--- Fibonacci.js
+++ Fibonacci.js
@@ -4,9 +4,9 @@ let n1 = 0, n2 = 1, next;
 let Fibonacci = () => {
     for(let i =0; i <= Number; i++) {
         console.log(n1);
-        n3 = n1 + n2;
+        next = n1 + n2;
         n1 = n2;
-        n2 = n3;
+        n2 = next;
     }
 }
 Fibonacci();
