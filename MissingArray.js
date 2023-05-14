a4cf12b6b7a83b64fd9b410eee1dba414f086094
Suryakanta Dash <sdash8273@gmail.com>
2023-05-14 22:53:43 +0530

    updated

--- MissingArray.js
+++ MissingArray.js
@@ -9,5 +9,11 @@
 let arr = [1, 2, 3, 4, 5, 6, 7, 9, 10];
 let Misnum = ((arr.length + 1) * (arr.length + 2)) / 2;
 
+for(let i = 0; i < arr.length; i++){
+    Misnum = Misnum - arr[i];
+}
+
+console.log(Misnum);
+
 
 
