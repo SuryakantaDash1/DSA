3c69c25e0934f41263afd14f891513d49d183216
Suryakanta Dash <sdash8273@gmail.com>
2023-05-14 22:52:21 +0530

    updated

--- MissingArray.js
+++ MissingArray.js
@@ -1,9 +1,13 @@
-let arr = [1, 2, 3, 4, 6, 7, 8, 9, 10];
-let misnum = ((arr.length + 1) * (arr.length + 2)) / 2;
+// let arr = [1, 2, 3, 4, 6, 7, 8, 9, 10];
+// let misnum = ((arr.length + 1) * (arr.length + 2)) / 2;
 
-for( let i = 0; i < arr.length; i++) {
-    misnum = misnum - arr[i];
-}
-console.log(misnum);
+// for( let i = 0; i < arr.length; i++) {
+//     misnum = misnum - arr[i];
+// }
+// console.log(misnum);
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 9, 10];
+let Misnum = ((arr.length + 1) * (arr.length + 2)) / 2;
+
 
 
