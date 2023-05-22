c347e3fd9f702c8f8097f9204fd9b11c6cfe039a
Suryakanta Dash <sdash8273@gmail.com>
2023-05-22 09:45:13 +0530

    updated

--- Larg.js
+++ Larg.js
@@ -1,5 +1,5 @@
 let arr = [2, 4, 1, 6, 8, 9, 5];
-let larg = Number.MIN_VALUE;
+let large = Number.MIN_VALUE;
 let secondLarg = Number.MIN_VALUE;
 for(let i =0; i < arr.length; i++){
     if(larg < arr[i]){
