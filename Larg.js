9a228d5300e3458c90c7e25b2c6ebbba6ae22a3f
Suryakanta Dash <sdash8273@gmail.com>
2023-05-22 09:47:25 +0530

    updated

--- Larg.js
+++ Larg.js
@@ -2,11 +2,11 @@ let arr = [2, 4, 1, 6, 8, 9, 5];
 let large = Number.MIN_VALUE;
 let secondLarg = Number.MIN_VALUE;
 for(let i =0; i < arr.length; i++){
-    if(larg < arr[i]){
-        secondLarg = larg;
-        larg = arr[i];
+    if(large < arr[i]){
+        secondLarg = large;
+        large = arr[i];
     }
-    if(secondLarg < arr[i] && larg > arr[i]){
+    if(secondLarg < arr[i] && large > arr[i]){
         secondLarg = arr[i];
     }
 }
