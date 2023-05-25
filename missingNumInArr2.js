2cd54dcf141cf5fe10c51c4eb8a0d9ffa47ddaf6
Suryakanta Dash <sdash8273@gmail.com>
2023-05-25 11:43:30 +0530

    missing Num

---
+++ missingNumInArr2.js
@@ -0,0 +1,11 @@
+let arr = [1,2,3,4,5,6,7,8,10];
+const findMissingNum = (arr) => {
+  for(var i = 0; i < arr.length - 1; i++) {
+    if(arr[i] + 1 != arr[i+1] ) {
+      return arr[i] + 1;
+    }
+  }
+  
+  return false;
+}
+console.log(findMissingNum(arr));
