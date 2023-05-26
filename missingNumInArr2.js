dfb9ee7ce6133579a2475eb33b72465d0a167689
Suryakanta Dash <sdash8273@gmail.com>
2023-05-26 11:00:45 +0530

    updated

--- missingNumInArr2.js
+++ missingNumInArr2.js
@@ -1,5 +1,5 @@
 let arr = [1,2,3,4,6,7,8,9,10];
-const findMissingNum = (arr) => {
+const MissingNum = (arr) => {
   for(var i = 0; i < arr.length - 1; i++) {
     if(arr[i] + 1 != arr[i+1] ) {
       return arr[i] + 1;
