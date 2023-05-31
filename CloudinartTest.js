149566024b8afd40b6f3d1b4a84d2a7a8af5f5c0
Suryakanta Dash <sdash8273@gmail.com>
2023-05-31 21:52:16 +0530

    updated

---
+++ CloudinartTest.js
@@ -0,0 +1,25 @@
+// File: cloudinaryTest.js
+
+import cloudinary from 'cloudinary';
+
+// Configure Cloudinary
+cloudinary.config({
+  cloud_name: 'dkwbh6ri7',
+  api_key: '532191727763865',
+  api_secret: 'owMHXRa3DHJj6FrBHenOkmLQVug',
+});
+
+// Perform a test upload
+const uploadTest = async () => {
+  try {
+    const result = await cloudinary.uploader.upload('path_to_your_test_image.jpg', {
+      folder: 'images',
+    });
+    console.log(result);
+  } catch (error) {
+    console.error('Error uploading test image:', error);
+  }
+};
+
+// Run the test
+uploadTest();
--- ThirdSmallestNoInArray.js
+++ ThirdSmallestNoInArray.js
@@ -7,5 +7,5 @@ numbers = [2, 3, 5, 7, 1]
 console.log("Original array : " + numbers)
 console.log("Third smallest number: "+test(numbers));
 numbers = [2, 3, 0, 5, 7, 8, -2, -4]
-console.log("Original arrays: "+ nums)
+console.log("Original arrays: "+ numbers)
 console.log("Third smallest number: "+test(numbers));
