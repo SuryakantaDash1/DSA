ea411f61906920cb98067c2002ccdca6f84b69ef
Suryakanta Dash <sdash8273@gmail.com>
2023-06-02 10:45:30 +0530

    updated

--- CloudinartTest.js
+++ CloudinartTest.js
@@ -1,14 +1,13 @@
 
 import cloudinary from 'cloudinary';
 
-// Configure Cloudinary
 cloudinary.config({
   cloud_name: 'dkwbh6ri7',
   api_key: '532191727763865',
   api_secret: 'owMHXRa3DHJj6FrBHenOkmLQVug',
 });
 
-// Perform a test upload
+
 const uploadTest = async () => {
   try {
     const result = await cloudinary.uploader.upload('path_to_your_test_image.jpg', {
