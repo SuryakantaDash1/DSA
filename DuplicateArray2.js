d7b8296a499622731c19c67df2d2e639332d2db5
Suryakanta Dash <sdash8273@gmail.com>
2023-06-10 16:03:41 +0530

    updated

--- DuplicateArray2.js
+++ DuplicateArray2.js
@@ -1,4 +1,4 @@
-const arr = [1,1,2,3,4,5,6,7,8,6,6,7,7,7,10,10];
+const arr = [1,1,2,3,4,5,6,7,8,6,6,7,7,7,8,9,10,10];
 const returnMultipleDupesArray = (arr) => {
   let observed = {};
   let dupesArray = [];
