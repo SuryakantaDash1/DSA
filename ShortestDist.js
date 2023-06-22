6f1f0be2c11535bb6d8f0ec3541ae7406e9ff7c3
Suryakanta Dash <sdash8273@gmail.com>
2023-06-22 11:34:44 +0530

    updated

--- ShortestDist.js
+++ ShortestDist.js
@@ -1,17 +1,3 @@
-// function getshortestDist(inputdist) {
-//     var distances = inputdist.split(" ");
-//     var m = parseInt(distances[0]);
-//     var n = parseInt(distances[1]);
-  
-//     var distance = Math.sqrt(m * m + n * n);
-//     return distance;
-//   }
-  
-//   var inputdist = process.openStdin();
-//   var distance = getshortestDist(inputdist);
-//   console.log(distance.toFixed(2));
-  
-
 function getshortestDist(inputdist) {
     var distances = inputdist.split(" ");
     var m = parseInt(distances[0]);
@@ -21,7 +7,21 @@ function getshortestDist(inputdist) {
     return distance;
   }
   
-  var inputdist = "3 4"; // Sample input provided as a string
+  var inputdist = process.openStdin();
   var distance = getshortestDist(inputdist);
   console.log(distance.toFixed(2));
+  
+
+// function getshortestDist(inputdist) {
+//     var distances = inputdist.split(" ");
+//     var m = parseInt(distances[0]);
+//     var n = parseInt(distances[1]);
+  
+//     var distance = Math.sqrt(m * m + n * n);
+//     return distance;
+//   }
+  
+//   var inputdist = "3 4"; // Sample input provided as a string
+//   var distance = getshortestDist(inputdist);
+//   console.log(distance.toFixed(2));
   
