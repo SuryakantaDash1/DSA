6048a437602f464c4b07f2caac5a3a45a66c4a80
Suryakanta Dash <sdash8273@gmail.com>
2023-06-26 18:33:50 +0530

    updated

--- SmallestNoInArray.js
+++ SmallestNoInArray.js
@@ -1,8 +1,8 @@
 let arr = [4, 5, 2, 8, 9, 7];
 let SmallNo = Number.MAX_VALUE;
 for(let i = 0; i<arr.length; i++){
-    if(Small > arr[i]){
-        Small = arr[i];
+    if(SmallNo > arr[i]){
+        SmallNo = arr[i];
     }
 }
 console.log(Small);
